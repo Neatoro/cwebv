3d75ba63f09f559034b60db9bce68d3cc8dd1069
Moritz Schramm <moritz.schramm@gmx.de>
2024-11-22 16:52:56 +0100

    Basic socket server implementation

--- include/http.h
+++ include/http.h
@@ -1,2 +1,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+
+int create_http_server(int port);
+
+void start_server(int server);
--- src/http.c
+++ src/http.c
@@ -1,6 +1,57 @@
 #include "http.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int create_http_server(int port) {
+  struct sockaddr_in server;
+
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  memset(&server, 0, sizeof(server));
+
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = htonl(INADDR_ANY);
+  server.sin_port = htons(port);
+
+  if (bind(sock, (struct sockaddr*)&server, sizeof(server)) < 0) {
+    return -1;
+  }
+
+  return sock;
+}
+
+void start_server(int server) {
+  struct sockaddr_in client;
+  unsigned int len;
+  
+  listen(server, 10);
+
+  printf("Server ready\n");
+
+  for (;;) {
+    len = sizeof(client);
+    int connection = accept(server, (struct sockaddr*)&client, &len);
+
+    if (connection < 0) {
+      printf("Failed to accept connection\n");
+      continue;
+    }
+
+    char buffer[1025];
+    snprintf(buffer, sizeof(buffer), "Hello from Server\n");
+    write(connection, buffer, strlen(buffer));
+
+    close(connection);
+  }
+}
 
 int main(int argc, char *argv[]) {
-  printf("Hello World");
+  int server = create_http_server(8080);
+  if (server < -1) {
+    printf("Server failed to bind\n");
+    return -1;
+  }
+
+  start_server(server);
 }
